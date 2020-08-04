5ca7c16c9cfd50bed19773d671a358713f6b1722
IHprogram <ihm20202020@gmail.com>
2020-08-04 12:42:09 +0900

    カラム名の修正

--- db/migrate/20200710140056_create_posts.rb
+++ db/migrate/20200710140056_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|   # 「実際にpostsテーブルを作成する。」という仕様が記載されている
-      t.text :content              # memoカラムを追加。 t.に続くのが、カラムの型(どんなデータが入るのかを示している)　「t.カラムの型　:カラム名」
+      t.text :memo              # memoカラムを追加。 t.に続くのが、カラムの型(どんなデータが入るのかを示している)　「t.カラムの型　:カラム名」
       t.timestamps
     end
   end
